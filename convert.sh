56fdc36c810ef328a506d75e09c9e936b110771b
reginaldobarreto <reginaldo.barreto@gmail.com>
2019-12-13 15:00:23 -0300

    db-update - '13.12.19'

--- convert.sh
+++ convert.sh
@@ -9,14 +9,46 @@ at now + 15 minutes -f convert.sh
 
 # echo Parado
 
+
+for i in *\(*\)*; do j=`echo $i |sed -e 's/[()]//g'`; mv "$i" "$j"; done
+rename 'y/[comandotorrents.com].//' *
+rename 'y/ /./' *
+rename 'y/A-Z/a-z/' *.*
+rename 's/á/a/g' *.*
+rename 's/ã/a/g' *.*
+rename 's/â/a/g' *.*
+rename 's/à/a/g' *.*
+rename 's/é/e/g' *.*
+rename 's/ẽ/e/g' *.*
+rename 's/ê/e/g' *.*
+rename 's/í/i/g' *.*
+rename 's/õ/o/g' *.*
+rename 's/ó/o/g' *.*
+rename 's/ú/u/g' *.*
+rename 's/ù/u/g' *.*
+rename 's/Á/a/g' *.*
+rename 's/Ã/a/g' *.*
+rename 's/Â/a/g' *.*
+rename 's/À/a/g' *.*
+rename 's/É/e/g' *.*
+rename 's/Ẽ/e/g' *.*
+rename 's/Ê/e/g' *.*
+rename 's/Í/i/g' *.*
+rename 's/Õ/o/g' *.*
+rename 's/Ó/o/g' *.*
+rename 's/Ú/u/g' *.*
+rename 's/Ù/u/g' *.*
+rename 's/ç/c/g' *.*
+
+
 for file in $( find ./ -iname "*.mkv" -o -iname "*.avi")
 	do
   OIFS="$IFS"
   IFS=$'\n'
 # usado em caso de audio saindo em ingles - menor velocidade #
-#  ffmpeg -i "$file" -vcodec copy -acodec aac -strict -2 "${file%.*}.mp4"
+  ffmpeg -i "$file" -vcodec copy -acodec aac -strict -2 "${file%.*}.mp4"
 # usado apenas para mudar o formato do arquivo - melhor velocidade #
-  ffmpeg -i "$file" -codec copy "${file%.*}.mp4"
+#  ffmpeg -i "$file" -codec copy "${file%.*}.mp4"
   mv "${file%.*}.mp4" /home/debian-media/Vídeos/Filmes/
   rm "$file"
   IFS="$OIFS"
